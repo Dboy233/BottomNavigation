9993fe8bb8b40c0edef26718de7fdc09e4dd45b8
Dboy <894230813@qq.com>
2021-04-15 14:58:27 +0800

    变量重命名

--- app/src/main/java/com/example/bottom/comm/LottieRawMenuSelector.kt
+++ app/src/main/java/com/example/bottom/comm/LottieRawMenuSelector.kt
@@ -9,13 +9,13 @@ import com.example.bottom.navigation.MenuSelector
  * @author DBoy 2021/2/1
  * <br/>    文件描述 : lottie动画菜单选择控制器
  */
-class LottieRawMenuSelector(@RawRes lottieRawId: Int) :
+class LottieRawMenuSelector(@RawRes val lottieRawId: Int) :
     MenuSelector<Int>(lottieRawId, lottieRawId) {
 
     override fun attachedToWindow(view: CompatMenuView) {
         if (view.iconView is LottieAnimationView) {
             (view.iconView as LottieAnimationView).apply {
-                setAnimation(selectorRes)
+                setAnimation(lottieRawId)
             }
         }
     }
